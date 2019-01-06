3b54a311c91f452210504420cf11c95a95ab4930
OzerAdem <43863411+OzerAdem@users.noreply.github.com>
2019-01-07 00:17:05 +0300

    Migrations Yüklendi

---
+++ migrations/m190106_125349_grupgruplama.php
@@ -0,0 +1,53 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m190106_125349_grupgruplama
+ */
+class m190106_125349_grupgruplama extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function Up()
+    {
+        $this->createTable('gruplar', [
+            'grupid' => $this->primaryKey(),
+            'grupadi' => $this->string(255)->notNull(),
+        ]);
+
+        $this->createTable('kayit', [
+            'kayitid' => $this->primaryKey(),
+            'ad' => $this->string(25)->notNull(),
+            'soyad' => $this->string(25)->notNull(),
+            'email' => $this->string(255)->notNull(),
+            'numara' => $this->string(25)->notNull(),
+            'grup' => $this->string(7)->notNull(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function Down()
+    {
+        $this->dropTable('gruplar');
+        $this->dropTable('kayit');
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m190106_125349_grupgruplama cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
